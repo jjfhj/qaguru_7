120ba82d3ccd9b36c7353f1f40c603ff06f9185b
Karina <aiwpront@gmail.com>
2021-11-15 20:48:23 +0400

    added download pdf file

--- src/test/java/com/github/jjfhj/FilesTest.java
+++ src/test/java/com/github/jjfhj/FilesTest.java
@@ -1,6 +1,10 @@
 package com.github.jjfhj;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Configuration;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,9 +17,13 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilesTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     @DisplayName("Загрузка файла формата rtf")
@@ -32,5 +40,15 @@ public class FilesTest {
         File download = $(byText("Договор о практической подготовке (долгосрочный)")).download();
         String fileContent = IOUtils.toString(new FileReader(download));
         assertFalse(fileContent.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Скачивание PDF файла и проверка его содержимого")
+    void checkingContentOfDownloadedPDFFile() throws IOException {
+        open("https://canadamania.ru/uslugi/platnaya-onlayn-konsultatsiya/");
+        File pdf = $("[title='Initial Consultation Template']").download();
+        PDF parsedPdf = new PDF(pdf);
+        Assertions.assertEquals("Mihaela", parsedPdf.author);
+        Assertions.assertEquals(1, parsedPdf.numberOfPages);
     }
 }
