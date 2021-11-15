0a9351d6acdce6d647ef8c8e295888328735bc8c
Karina <aiwpront@gmail.com>
2021-11-15 17:57:02 +0400

    added file upload

--- src/test/java/com/github/jjfhj/FilesTest.java
+++ src/test/java/com/github/jjfhj/FilesTest.java
@@ -1,4 +1,19 @@
 package com.github.jjfhj;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FilesTest {
+
+    @Test
+    @DisplayName("Загрузка файла формата rtf")
+    void checkingFilenameAfterUpload() {
+        open("https://demoqa.com/upload-download");
+        $("#uploadFile").uploadFromClasspath("example.rtf");
+        $("#uploadedFilePath").shouldHave(text("example.rtf"));
+    }
 }
